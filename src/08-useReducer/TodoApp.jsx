edc63b8306ef34f6d7b579a086085e49a3645b3b
nukkua <marco.salazar.2014.com@gmail.com>
2024-01-06 16:28:57 -0400

    done

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,41 +1,19 @@
-import { useEffect, useReducer } from 'react';
-import { todoReducer } from './todoReducer';
 import { TodoList } from './TodoList';
 import { TodoAdd } from './TodoAdd';
-
-const initialState = [];
-
-const init = () => {
-	return JSON.parse(localStorage.getItem('todos')) || [];
-};
+import { useTodo } from '../hooks/useTodo';
 
 export const TodoApp = () => {
-	const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-
-	useEffect(() => {
-		localStorage.setItem('todos', JSON.stringify(todos));
-	}, [todos]);
-
-	const handleNewTodo = (todo) => {
-		const action = {
-			type: '[TODO] Add Todo',
-			payload: todo
-		};
-		dispatch(action);
-	};
-	const handleDeleteTodo = (id) => {
-		dispatch({ type: '[TODO] Remove Todo', payload: id });
-	};
-
+	const { todos, handleNewTodo, handleDeleteTodo, handleToggleTodo, todosCount, pedingTodosCount } =
+		useTodo();
 	return (
 		<>
 			<h1>
-				Todo App: 10 <small>pendientes: 2</small>
+				Todo App: {todosCount} <small>pendientes: {pedingTodosCount}</small>
 			</h1>
 			<hr />
 			<div className="row">
 				<div className="col-7">
-					<TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
+					<TodoList todos={todos} onDeleteTodo={handleDeleteTodo} onToggleTodo={handleToggleTodo} />
 				</div>
 				<div className="col-5">
 					<h4>Add TODO</h4>
--- src/08-useReducer/TodoItem.jsx
+++ src/08-useReducer/TodoItem.jsx
@@ -1,7 +1,12 @@
-export const TodoItem = ({ todo, onDeleteTodo }) => {
+export const TodoItem = ({ todo, onDeleteTodo, onToggleTodo }) => {
 	return (
 		<li className="list-group-item d-flex justify-content-between">
-			<span className="align-self-center">{todo.description}</span>
+			<span
+				onClick={() => onToggleTodo(todo.id)}
+				className={`align-self-center ${todo.done ? 'text-decoration-line-through' : ''}`}
+			>
+				{todo.description}
+			</span>
 			<button onClick={() => onDeleteTodo(todo.id)} className="btn btn-danger">
 				Borrar
 			</button>
